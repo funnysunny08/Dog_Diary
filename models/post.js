b6115133a39494c90c830a6007651435557fc111
funnysunny08 <rosa7536@naver.com>
2022-11-24 14:28:39 +0900

    [INIT]

---
+++ models/post.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Post extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init({
+            title: {
+                type: Sequelize.STRING(100),
+                allowNull: false,
+            },
+            content: {
+                type: Sequelize.TEXT,
+                allowNull: false,
+            },
+            img: {
+                type: Sequelize.STRING(200),
+                allowNull: true,
+            },
+            date: {
+                type: Sequelize.DATEONLY,
+                allowNull: false,
+                defaultValue: Sequelize.NOW,
+            },
+        }, {
+            sequelize,
+            timestamps: true,
+            underscored: false,
+            modelName: 'Post',
+            tableName: 'posts',
+            paranoid: false,
+            charset: 'utf8mb4',
+            collate: 'utf8mb4_general_ci',
+        });
+    }
+
+    static associate(db) {
+        db.Post.belongsTo(db.User);
+    }
+};
---
+++ routes/page.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const { Post, User } = require('../models');
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+  res.locals.user = req.user;
+  res.locals.followerCount = req.user ? req.user.Followers.length : 0;
+  res.locals.followingCount = req.user ? req.user.Followings.length : 0;
+  res.locals.followerIdList = req.user ? req.user.Followings.map(f => f.id) : [];
+  next();
+});
+
+router.get('/profile', (req, res) => {
+  res.render('profile', { title: '내 정보 - NodeBird' });
+});
+
+//* 로그인
+router.get('/login', isNotLoggedIn, (req, res) => {
+    res.render('login', { title: '로그인' });
+});
+
+//* 회원가입
+router.get('/join', isNotLoggedIn, (req, res) => {
+  res.render('join', { title: '회원가입' });
+});
+
+//* 홈화면
+router.get('/', async (req, res, next) => {
+  const twits = [];
+  res.render('main', {
+    title: '홈화면',
+  });
+});
+
+//* 마이페이지
+router.get('/mypage', isLoggedIn, async (req, res) => {
+    res.render('mypage', { title: '마이페이지' });
+});
+
+//* 내개
+router.get('/mydog', async (req, res) => {
+    res.render('mydog', { title: '내개' });
+});
+
+//* 니개
+router.get('/yourdog', async (req, res) => {
+  try {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        attributes: ['id', 'nick'],
+      },
+      order: [['createdAt', 'DESC']],
+    });
+    res.render('yourdog', { title: '니개', posts: posts });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }    
+});
+
+module.exports = router;
